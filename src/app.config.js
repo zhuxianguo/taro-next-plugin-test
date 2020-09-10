2e41084678a53717ffe92e1be5e9b29eddf06a77
zhuxianguo <zhuxianguo@hztianque.com>
2020-09-10 19:52:51 +0800

    init

---
+++ src/app.config.js
@@ -0,0 +1,17 @@
+export default {
+  plugins: {
+    "wxparserPlugin": {
+      "version": "0.3.1",
+      "provider": "wx9d4d4ffa781ff3ac"
+    }
+  },
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
---
+++ src/pages/index/index.config.js
@@ -0,0 +1,6 @@
+export default {
+  navigationBarTitleText: '首页',
+  usingComponents: {
+    "wxparser": "plugin://wxparserPlugin/wxparser"
+  }
+}
